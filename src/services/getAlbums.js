ef6c3ef1088c98ef8b5350cb1120566128dd9aac
viviane.wu.mac <viviane.wu.mac@axa.com.hk>
2018-08-06 02:14:14 +0800

    fix style of show comment text

--- src/services/getAlbums.js
+++ src/services/getAlbums.js
@@ -3,8 +3,7 @@ import axios from 'axios';
 
 export default async function getAlbums(userId) {
   try {
-    const result = await axios.get(`${config.fakeRestAPI}/albums?userId=${userId}
-    `);
+    const result = await axios.get(`${config.fakeRestAPI}/albums?userId=${userId}`);
 
     return result.data;
   } catch (e) {
